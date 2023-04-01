644f161585af5809205362f97f4609ef77383821
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-04-01 22:46:38 +0300

    працюэ

---
+++ src/index.js
@@ -0,0 +1,88 @@
+import './css/styles.css';
+import { pixabayAPI } from './js/fetchPhoto.js';
+import Notiflix from 'notiflix';
+const formEl = document.querySelector('.search-form');
+const inputEl = document.querySelector('.input');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreEl = document.querySelector('.load-more');
+
+const pixabayApi = new pixabayAPI();
+let total = 0;
+const handleSubmit = async event => {
+  event.preventDefault();
+  total = 0;
+  total += pixabayApi.perPage;
+  console.log(total);
+  if (inputEl.value === '') {
+    return;
+  }
+  pixabayApi.page = 1;
+  pixabayApi.photo = inputEl.value.trim();
+  try {
+    const { data } = await pixabayApi.fetchPhoto();
+    console.log(data);
+    const arrayPhoto = data.hits;
+    galleryEl.innerHTML = makePhoto(arrayPhoto);
+    if (data.totalHits === 0) {
+      loadMoreEl.classList.add('is-hidden');
+      return Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else if (data.totalHits < pixabayApi.perPage || data.totalHits === 0) {
+      loadMoreEl.classList.add('is-hidden');
+      galleryEl.insertAdjacentHTML(
+        'beforeend',
+        `"We're sorry, but you've reached the end of search results."`
+      );
+      return;
+    }
+    loadMoreEl.classList.remove('is-hidden');
+  } catch (error) {
+    console.log(error);
+  }
+};
+function makePhoto(arrayPhoto) {
+  const photo = arrayPhoto
+    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+      return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300px"height="300px"/>
+  <div class="info">
+    <p class="info-item">
+      <b>likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>downloads ${downloads}</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+  return photo;
+}
+const handleloadMore = async () => {
+  pixabayApi.page += 1;
+  try {
+    const { data } = await pixabayApi.fetchPhoto();
+    total += pixabayApi.perPage;
+    console.log(total);
+    const arrayPhoto = data.hits;
+    galleryEl.insertAdjacentHTML('beforeend', makePhoto(arrayPhoto));
+    if (data.totalHits < total) {
+      loadMoreEl.classList.add('is-hidden');
+      galleryEl.insertAdjacentHTML(
+        'beforeend',
+        `"We're sorry, but you've reached the end of search results."`
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+formEl.addEventListener('submit', handleSubmit);
+loadMoreEl.addEventListener('click', handleloadMore);
--- src/js/fetchPhoto.js
+++ src/js/fetchPhoto.js
@@ -1,47 +1,24 @@
 import axios from 'axios';
+BASE_URL = 'https://pixabay.com/api';
+API_KEY = '34957415-93d84ab2394a2a56ca546a14b';
 
 export class pixabayAPI {
   photo = null;
   page = 1;
   perPage = 40;
-  async fetchPhoto() {
-    try {
-      return await axios.get(
-        `https://pixabay.com/api/?key=34957415-93d84ab2394a2a56ca546a14b&q=${this.photo}&per_page=${this.perPage}&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`
-      );
-    } catch (error) {
-      throw new Error(error.message);
-    }
+  async fetchPhoto() { try {
+        return await axios.get(
+          `${BASE_URL}/?key=${API_KEY}&q=${this.photo}&per_page=${
+            this.perPage
+          }&page=${
+            this.page
+          }&image_type=photo&orientation=horizontal&safesearch=true`
+        );
+      } catch (err) {
+        throw new Error(err.message);
+      }
   }
 }
 
-// #BASE_URL = 'https://pixabay.com/api';
-// #API_KEY = '34957415-93d84ab2394a2a56ca546a14b';
 
-// page = 1;
-// photo = null;
-// perPage = 40;
 
-// async fetchPhotos() {
-//   try {
-//     return await axios.get(
-//       `${this.#BASE_URL}/?key=${this.#API_KEY}&q=${this.photo}&per_page=${
-//         this.perPage
-//       }&page=${
-//         this.page
-//       }&image_type=photo&orientation=horizontal&safesearch=true`
-//     );
-//   } catch (err) {
-//     throw new Error(err.message);
-
-// return await axios.get(`${this.#BASE_URL}/search/photos`, {
-//   params: {
-//     q: this.q,
-//     page: this.page,
-//     per_page: 40,
-//     image_type: 'photo',
-//     orientation: horizontal,
-//     safesearch: true,
-//     client_id: this.#API_KEY,
-//   },
-// });
