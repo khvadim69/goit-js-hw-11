383bc77d1ac318a6a4956b23f985efdca6d496db
khvadim69 <101789017+khvadim69@users.noreply.github.com>
2023-04-02 10:03:57 +0300

    Update fetchPhoto.js

--- src/js/fetchPhoto.js
+++ src/js/fetchPhoto.js
@@ -1,24 +1,18 @@
 import axios from 'axios';
-BASE_URL = 'https://pixabay.com/api';
-API_KEY = '34957415-93d84ab2394a2a56ca546a14b';
+// BASE_URL = 'https://pixabay.com/api';
+// API_KEY = '34957415-93d84ab2394a2a56ca546a14b';
 
 export class pixabayAPI {
   photo = null;
   page = 1;
   perPage = 40;
-  async fetchPhoto() { try {
-        return await axios.get(`
-          ${BASE_URL}/?key=${API_KEY}&q=${this.photo}&per_page=${
-            this.perPage
-          }&page=${
-            this.page
-          }&image_type=photo&orientation=horizontal&safesearch=true`
-        );
-      } catch (err) {
-        throw new Error(err.message);
-      }
+  async fetchPhoto() {
+    try {
+      return await axios.get(`
+      https://pixabay.com/api/?key=34957415-93d84ab2394a2a56ca546a14b&q=${this.photo}&per_page=${this.perPage}&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`);
+    } catch (err) {
+      throw new Error(err.message);
+    }
   }
 }
-
-
-
+// ${BASE_URL}/?key=${API_KEY}
